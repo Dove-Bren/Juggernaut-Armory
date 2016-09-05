c8078e22d753d90a11f4a2eb89227bf77d88e799
Skyler Manzanares <skymanzanares@hotmail.com>
2016-09-04 22:21:39 -0600

    cleanup

--- src/main/java/com/SkyIsland/Armory/forge/Forge.java
+++ src/main/java/com/SkyIsland/Armory/forge/Forge.java
@@ -522,24 +522,20 @@ public class Forge extends BlockBase implements ITileEntityProvider {
 		
 		public ForgeContainer(IInventory playerInv, ForgeTileEntity forge) {
 			this.forge = forge;
-			short slotID = 0;
 			
 			// Construct slots for player to interact with
 			// Brazier only needs '1' inventory slot to interact with
 			// Uses slot ID 0
 			this.addSlotToContainer(new Slot(forge, 0, 124, 34));
-			slotID = 1;
 			// Construct player inventory
 			for (int y = 0; y < 3; y++) {
 				for (int x = 0; x < 9; x++) {
 					this.addSlotToContainer(new Slot(playerInv, x + y * 9 + 9, 8 + (x * 18), 84 + (y * 18)));
-					slotID++;
 				}
 			}
 			// Construct player hotbar
 			for (int x = 0; x < 9; x++) {
 				this.addSlotToContainer(new Slot(playerInv, x, 8 + x * 18, 142));
-				slotID++;
 			}
 		}
 		
