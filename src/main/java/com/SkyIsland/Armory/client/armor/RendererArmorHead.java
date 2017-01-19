667baf329ecebae2df1d04b486335394a1196c7a
Skyler Manzanares <skymanzanares@hotmail.com>
2017-01-18 20:17:02 -0700

    Added rotation on armor stands, and locals

--- src/main/java/com/SkyIsland/Armory/client/armor/RendererArmorHead.java
+++ src/main/java/com/SkyIsland/Armory/client/armor/RendererArmorHead.java
@@ -21,6 +21,7 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
@@ -684,6 +685,34 @@ public class RendererArmorHead extends ModelBiped {
 //		}
 
 		super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
+		
+		if (e instanceof EntityArmorStand)
+        {
+            EntityArmorStand entityarmorstand = (EntityArmorStand)e;
+            this.bipedHead.rotateAngleX = 0.017453292F * entityarmorstand.getHeadRotation().getX();
+            this.bipedHead.rotateAngleY = 0.017453292F * entityarmorstand.getHeadRotation().getY();
+            this.bipedHead.rotateAngleZ = 0.017453292F * entityarmorstand.getHeadRotation().getZ();
+            this.bipedHead.setRotationPoint(0.0F, 1.0F, 0.0F);
+            this.bipedBody.rotateAngleX = 0.017453292F * entityarmorstand.getBodyRotation().getX();
+            this.bipedBody.rotateAngleY = 0.017453292F * entityarmorstand.getBodyRotation().getY();
+            this.bipedBody.rotateAngleZ = 0.017453292F * entityarmorstand.getBodyRotation().getZ();
+            this.bipedLeftArm.rotateAngleX = 0.017453292F * entityarmorstand.getLeftArmRotation().getX();
+            this.bipedLeftArm.rotateAngleY = 0.017453292F * entityarmorstand.getLeftArmRotation().getY();
+            this.bipedLeftArm.rotateAngleZ = 0.017453292F * entityarmorstand.getLeftArmRotation().getZ();
+            this.bipedRightArm.rotateAngleX = 0.017453292F * entityarmorstand.getRightArmRotation().getX();
+            this.bipedRightArm.rotateAngleY = 0.017453292F * entityarmorstand.getRightArmRotation().getY();
+            this.bipedRightArm.rotateAngleZ = 0.017453292F * entityarmorstand.getRightArmRotation().getZ();
+            this.bipedLeftLeg.rotateAngleX = 0.017453292F * entityarmorstand.getLeftLegRotation().getX();
+            this.bipedLeftLeg.rotateAngleY = 0.017453292F * entityarmorstand.getLeftLegRotation().getY();
+            this.bipedLeftLeg.rotateAngleZ = 0.017453292F * entityarmorstand.getLeftLegRotation().getZ();
+            this.bipedLeftLeg.setRotationPoint(1.9F, 11.0F, 0.0F);
+            this.bipedRightLeg.rotateAngleX = 0.017453292F * entityarmorstand.getRightLegRotation().getX();
+            this.bipedRightLeg.rotateAngleY = 0.017453292F * entityarmorstand.getRightLegRotation().getY();
+            this.bipedRightLeg.rotateAngleZ = 0.017453292F * entityarmorstand.getRightLegRotation().getZ();
+            this.bipedRightLeg.setRotationPoint(-1.9F, 11.0F, 0.0F);
+            copyModelAngles(this.bipedHead, this.bipedHeadwear);
+        }
+		
 	}
 
 }
